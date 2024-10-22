33ded5b2b8271f77fb4717f6d3dd25b0693b5f1c
neo <neo532@126.com>
2024-10-22 11:35:08 +0800

    log level modify

--- logger/default.go
+++ logger/default.go
@@ -40,67 +40,67 @@ func (l *DefaultLogger) WithLevel(lv Level) (n Logger) {
 	}
 }
 
-func (l *DefaultLogger) isMatchLevel(lv Level) (b bool) {
-	return l.level > lv
+func (l *DefaultLogger) disabled(lv Level) (b bool) {
+	return lv < l.level
 }
 
 func (l *DefaultLogger) Debugf(c context.Context, format string, kvs ...interface{}) {
-	if l.isMatchLevel(LevelDebug) {
+	if l.disabled(LevelDebug) {
 		return
 	}
 	l.log.Log(c, LevelDebug, fmt.Sprintf(format, kvs...), l.globalArgs...)
 }
 func (l *DefaultLogger) Warnf(c context.Context, format string, kvs ...interface{}) {
-	if l.isMatchLevel(LevelWarn) {
+	if l.disabled(LevelWarn) {
 		return
 	}
 	l.log.Log(c, LevelWarn, fmt.Sprintf(format, kvs...), l.globalArgs...)
 }
 func (l *DefaultLogger) Infof(c context.Context, format string, kvs ...interface{}) {
-	if l.isMatchLevel(LevelInfo) {
+	if l.disabled(LevelInfo) {
 		return
 	}
 	l.log.Log(c, LevelInfo, fmt.Sprintf(format, kvs...), l.globalArgs...)
 }
 func (l *DefaultLogger) Errorf(c context.Context, format string, kvs ...interface{}) {
-	if l.isMatchLevel(LevelError) {
+	if l.disabled(LevelError) {
 		return
 	}
 	l.log.Log(c, LevelError, fmt.Sprintf(format, kvs...), l.globalArgs...)
 }
 func (l *DefaultLogger) Fatalf(c context.Context, format string, kvs ...interface{}) {
-	if l.isMatchLevel(LevelFatal) {
+	if l.disabled(LevelFatal) {
 		return
 	}
 	l.log.Log(c, LevelFatal, fmt.Sprintf(format, kvs...), l.globalArgs...)
 }
 
 func (l *DefaultLogger) Debug(c context.Context, message string, kvs ...interface{}) {
-	if l.isMatchLevel(LevelDebug) {
+	if l.disabled(LevelDebug) {
 		return
 	}
 	l.log.Log(c, LevelDebug, message, append(l.globalArgs, kvs...)...)
 }
 func (l *DefaultLogger) Warn(c context.Context, message string, kvs ...interface{}) {
-	if l.isMatchLevel(LevelWarn) {
+	if l.disabled(LevelWarn) {
 		return
 	}
 	l.log.Log(c, LevelWarn, message, append(l.globalArgs, kvs...)...)
 }
 func (l *DefaultLogger) Info(c context.Context, message string, kvs ...interface{}) {
-	if l.isMatchLevel(LevelInfo) {
+	if l.disabled(LevelInfo) {
 		return
 	}
 	l.log.Log(c, LevelInfo, message, append(l.globalArgs, kvs...)...)
 }
 func (l *DefaultLogger) Error(c context.Context, message string, kvs ...interface{}) {
-	if l.isMatchLevel(LevelError) {
+	if l.disabled(LevelError) {
 		return
 	}
 	l.log.Log(c, LevelError, message, append(l.globalArgs, kvs...)...)
 }
 func (l *DefaultLogger) Fatal(c context.Context, message string, kvs ...interface{}) {
-	if l.isMatchLevel(LevelFatal) {
+	if l.disabled(LevelFatal) {
 		return
 	}
 	l.log.Log(c, LevelFatal, message, append(l.globalArgs, kvs...)...)
