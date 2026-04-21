b2b7f31a6f9ed266058cf8f9197ef451e623c24d
neo <neo532@126.com>
2026-04-21 15:33:56 +0800

    month

--- util/freq.go
+++ util/freq.go
@@ -164,7 +164,7 @@ func (f *Freq) freq(pre string, ruleList []FreqRule, fn func(key string, expire,
 				r.Timezone = f.tz
 			}
 			now := time.Now().In(r.Timezone)
-			key = prekey + DurationThisWeek + strconv.Itoa(int(now.Month()))
+			key = prekey + DurationThisMonth + strconv.Itoa(int(now.Month()))
 			expire = time.Date(now.Year(), now.Month()+1, 0, 0, 0, 0, 0, r.Timezone).Unix() - now.Unix()
 
 		default:
